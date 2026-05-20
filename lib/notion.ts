186dd96913e9fa0461f4e22915cf8d16f34b0ccd
absales91 <absales91@gmail.com>
2026-05-20 17:09:57 +0530

    ..

--- lib/notion.ts
+++ lib/notion.ts
@@ -20,7 +20,7 @@ export async function getBlogs() {
 
         sorts: [
           {
-            property: "Date",
+            property: "PublishDate",
             direction: "descending",
           },
         ],
